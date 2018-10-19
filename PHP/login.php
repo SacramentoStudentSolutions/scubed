f9c97bfc0c905ac2054258cc458b7f93d3d1507b
zendejas831 <jn.zendejas@gmail.com>
2018-10-19 00:50:30 -0700

    Fixed the login

--- PHP/login.php
+++ PHP/login.php
@@ -1,29 +1,27 @@
 <?php
 if($_SERVER['REQUEST_METHOD']=='POST'){
-		$LastName ='';
-		$FirstName ='';
-		$LastName = $_POST['LastName'];
-		$FirstName = $_POST['FirstName'];
+    $username = $_POST['username'];
+    $password = $_POST['password'];
 
-		echo 'Username: ',$LastName,'<br>Password: ',$FirstName,'<br>';
-		if($LastName == '' || $FirstName == ''){
-		}else{
-			require_once('dbConnect.php');
-			$sql = "SELECT * FROM Login WHERE username='$FirstName' and password='$LastName'";
+    //echo 'Username: ',$username,'<br>Password: ',$password,'<br>';
+    if($username == '' || $password == ''){
+    }else{
+        require_once('dbConnect.php');
+        $sql = "SELECT * FROM Login WHERE username='$username' and password='$password'";
 
-			$check = mysqli_fetch_array(mysqli_query($con,$sql));
-			echo 'sql: ',$check;
+        $check = mysqli_fetch_array(mysqli_query($con,$sql));
+        //echo 'sql: ',$check;
 
-			if(isset($check)){
-				$url = 'http://athena.ecs.csus.edu/~scubed/lower_division.html';
-					header( "Location: $url" );
-			}else{
-				echo 'Oops! Please try again!';
-			}
-			
-			mysqli_close($con);
-		}
+        if(isset($check)){
+            $url = 'http://athena.ecs.csus.edu/~scubed/lower_division.html';
+            header( "Location: $url" );
+        }else{
+            echo 'Oops! Please try again!';
+        }
+
+        mysqli_close($con);
+    }
 }else{
-	echo 'Error';
+    echo 'Error';
 }
 ?>
