dbf2b9a5c3b532aa7cf611635ada7eba53cc6787
SacramentoStudentSolutions <SacramentoStudentSolutions@gmail.com>
2018-10-29 17:48:34 -0700

    FPDF Tutorial

--- PHP/fpdf181/test.php
+++ PHP/fpdf181/test.php
@@ -3,14 +3,12 @@
  * Created by PhpStorm.
  * User: Jed
  * Date: 10/29/2018
- * Time: 5:42 PM
+ * Time: 5:44 PM
  */
 
-ob_start();
 require('fpdf.php');
-$pdf = new FPDF();
+$pdf= new FPDF();
 $pdf->AddPage();
-$pdf->SetFont('Arial','B',16);
-$pdf->Cell(40,10,'Hello World!');
-$pdf->Output();
-ob_end_flush();
+$pdf->SetFont('Arial','B',18);
+$pdf->Cell(40,10,'THIS IS A TEST');
+$pdf->output();
