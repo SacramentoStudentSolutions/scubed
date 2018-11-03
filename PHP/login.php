80cdd12aea84b5c42fc1d841ff298503714ae8d9
zendejas831 <jn.zendejas@gmail.com>
2018-11-02 18:35:06 -0700

    End of Sprint 4

--- PHP/login.php
+++ PHP/login.php
@@ -5,6 +5,7 @@ if($_SERVER['REQUEST_METHOD']=='POST'){
 
     //echo 'Username: ',$username,'<br>Password: ',$password,'<br>';
     if($username == '' || $password == ''){
+		// something is wrong
     }else{
         require_once('dbConnect.php');
         $sql = "SELECT * FROM Login2 WHERE username='$username' and password='$password'";
@@ -16,14 +17,31 @@ if($_SERVER['REQUEST_METHOD']=='POST'){
             $sql = "SELECT  admin_flag 
                     FROM Login2 
                     WHERE username='$username' and password='$password'";
+			$result = mysqli_query($con, $sql);
+			if (mysqli_num_rows($result) > 0) {
+				while($row = mysqli_fetch_assoc($result)) {
+					//global $course, $units, $semester, $grade, $notes;
+					$admin_flag = $row["admin_flag"];
+				}
+			}
             // Redirect to Student Page
-            if ($sql==1) {
-                $url = 'http://athena.ecs.csus.edu/~scubed/lower_division.html';
+
+            if ($admin_flag==0) {
+                $url = 'http://athena.ecs.csus.edu/~scubed/student_home.html';
                 header("Location: $url");
+                //$message = "wrong answer";
+				//$value="Value: ".$value;
+				//$value="hello world";
+                //echo "<script type='text/javascript'>alert('admin_flag: $admin_flag');</script>";
+				//echo "Value: ".$value."<br>";
             } else {
                 // Redirect to Advisor Page
-                $url = 'http://athena.ecs.csus.edu/~scubed/lower_division.html';
+                $url = 'http://athena.ecs.csus.edu/~scubed/PHP/search_form2.php';
                 header( "Location: $url" );
+                //$value="Value: ".$value;
+				//$value="hello world2";
+                //echo "<script type='text/javascript'>alert('admin_flag: $admin_flag');</script>";
+				//echo "Value: ".$value."<br>";
             }
         }else{
             $message = 'Wrong username or password.';
